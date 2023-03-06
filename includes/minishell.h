e0bb026bea9754d843ff685886258907b2fa4e10
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-06 15:27:07 -0300

    feat: linked_list data structure added to header

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 10:17:56 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/06 15:19:53 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/06 15:21:50 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef MINISHELL_H
@@ -26,5 +26,11 @@
 # include <sys/stat.h>
 # include <sys/types.h>
 # include <sys/ioctl.h>
+
+typedef struct s_node
+{
+	char			*data;
+	struct s_node	*next;
+}	t_node;
 
 #endif

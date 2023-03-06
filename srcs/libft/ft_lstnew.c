a66adb1575d960b6d3aa6d7680cfd329238e9ba6
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-06 16:27:56 -0300

    fix: now duping the content, since it's a string

--- srcs/libft/ft_lstnew.c
+++ srcs/libft/ft_lstnew.c
@@ -6,19 +6,19 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 12:37:02 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/06 15:40:33 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/06 16:21:02 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-t_node	*ft_lstnew(void *content)
+t_node	*ft_lstnew(char *content)
 {
 	t_node	*new_node;
 
 	new_node = (malloc(sizeof(t_node)));
 	if (!new_node)
 		return (0);
-	new_node->data = content;
+	new_node->data = ft_strdup(content);
 	new_node->next = NULL;
 	return (new_node);
 }

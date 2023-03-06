cc6d8e1addfe9f36fc4952666bf56cf32472dfb3
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-06 16:29:38 -0300

    feat: converting envp into linked list, no leaks

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -1,18 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 10:12:01 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/06 10:14:38 by revieira         ###   ########.fr       */
+/*   Updated: 2023/03/06 16:22:32 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
+#include "../includes/minishell.h"
+#include "libft/libft.h"
 
-int main()
+t_node	*get_envp_list(char **envp)
 {
-	printf("World! Hello\n");
+	int		i;
+	t_node	*new_node;
+	t_node	*envp_list;
+
+	i = 0;
+	envp_list = NULL;
+	while (envp[i] != NULL)
+	{
+		new_node = ft_lstnew(envp[i]);
+		ft_lstadd_back(&envp_list, new_node);
+		i++;
+	}
+	return (envp_list);
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	int		i;
+	t_node	*list;
+
+	i = 0;
+	if (argc || argv)
+	{
+		list = get_envp_list(envp);
+		ft_print_stack(list);
+		ft_free_list(&list);
+	}
 	return (0);
 }

bbc973b0cee58df3873d70f21d100e10a4be1406
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-07 15:57:29 -0300

    feat: transforming envp into linked list successfully and freeing properly

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,16 +6,24 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 10:12:01 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/06 19:09:25 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/07 15:45:53 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/minishell.h"
 #include "libft/libft.h"
 
-int main()
 {
+int main(int argc, char **argv, char **envp)
+{
+	t_node	*envp_list;
 	char	*line;
 
+	if (argc && argv)
+	{}
+	envp_list = get_envp_list(envp);
+	ft_print_stack(envp_list);
+	ft_free_list(&envp_list);
+	exit(0);
 	while (1)
 	{
 		line = readline("$ ");
@@ -28,5 +36,6 @@ int main()
 		}
 		free(line);
 	}
+	ft_free_list(&envp_list);
 	return (0);
 }

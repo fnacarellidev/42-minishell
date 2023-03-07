d8a0f4a9cc9b9fd5b1e9e365e7a5a87fad2a5993
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-07 18:14:00 -0300

    feat: is_valid_instruction

--- srcs/minicd.c
+++ srcs/minicd.c
@@ -56,3 +56,30 @@ static void	change_value_from_key(t_node **envp_list, char *key, char *new_data)
 		free(tmp->value);
 	tmp->value = ft_strdup(new_data);
 }
+
+static int	is_valid_instruction(t_node **envp_list, char *cmd)
+{
+	int		ret;
+	char	**cmd_tokens;
+	int		number_of_tokens;
+
+	ret = 1;
+	cmd_tokens = ft_split(cmd, ' ');
+	number_of_tokens = ft_count_matrix((void **)cmd_tokens);
+	if (number_of_tokens > 2)
+	{
+		printf("bash: cd: too many arguments");
+		ret = 0;
+	}
+	else if (!key_exists(*envp_list, "PWD="))
+		export(envp_list, "PWD=", NULL);
+	else if (!key_exists(*envp_list, "OLDPWD="))
+		export(envp_list, "OLDPWD=", NULL);
+	else if (!is_valid_dir(cmd_tokens[1]))
+	{
+		printf("bash: cd: %s: No such file or directory\n", cmd_tokens[1]);
+		ret = 0;
+	}
+	ft_free_matrix((void **)cmd_tokens);
+	return (ret);
+}

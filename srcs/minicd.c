73679bc46b1d54b624ac3b6b3bc433ce70dc895b
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-08 15:20:24 -0300

    refactor: renamed is_valid_dir param to path

--- srcs/minicd.c
+++ srcs/minicd.c
@@ -6,16 +6,16 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 19:16:11 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/07 18:04:20 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/08 15:18:01 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/minishell.h"
 
-static int	is_valid_dir(char *str)
+static int	is_valid_dir(char *path)
 {
 	int	check;
 
-	check = access(str, F_OK);
+	check = access(path, F_OK);
 	if (check == 0)
 		return (1);
 	else

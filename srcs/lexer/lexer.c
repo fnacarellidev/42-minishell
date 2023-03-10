02dc5e834482757b746e6b4a25c199935b38116f
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-10 20:20:17 -0300

    feat: put_spaces, further information at the top of function

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 18:57:20 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/10 19:49:05 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/10 20:19:46 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
@@ -38,4 +38,33 @@ static int	count_metachars(char *cmd)
 		cmd++;
 	}
 	return (amount_of_metachars);
+}
+
+// Receives the pointer to the position of a metacharacter, changed previous
+// and next position to spaces if it needs to, but before doing that, it 
+// shifts the string so it won't lose any data when writing the ' ' char
+int	put_spaces(char *str, int position_of_whole_string)
+{
+	int	i;
+
+	i = 0;
+	if (position_of_whole_string != 0 && *(str - 1) != ' ')
+	{
+		ft_memmove(str + 1, str, ft_strlen(str));
+		str[0] = ' ';
+		i++;
+		if (str[2] != ' ')
+		{
+			ft_memmove(str + 3, str + 2, ft_strlen(str));
+			str[2] = ' ';
+			i++;
+		}
+	}
+	else if (str[1] != ' ')
+	{
+		ft_memmove(str + 2, str + 1, ft_strlen(str));
+		str[1] = ' ';
+		i++;
+	}
+	return (i);
 }

42123188806fc25a3fb626332189e3eb3b1badcf
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-22 17:00:28 -0300

    feat: get_envp_list prototype added to header

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,10 +6,9 @@
 /*   By: fnacarel <fnacarel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 10:17:56 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/10 14:59:08 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/22 16:52:48 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -36,5 +35,7 @@ char	*get_key_value(t_node *envp_list, char *key);
 void	change_value_from_key(t_node **envp_list, char *key, char *new_data);
 int		key_exists(t_node *envp_list, char *key);
 char    **lexer(char *cmd);
+
+t_node	*get_envp_list(char **envp);
 
 #endif

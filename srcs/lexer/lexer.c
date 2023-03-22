533b80b85c84d6d956b535187d60679532bd786d
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-22 20:13:48 -0300

    feat: putting spaces between metachars correctly

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,13 +6,17 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 18:57:20 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/22 18:49:08 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/22 20:13:29 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
 char	**lexer(char *cmd)
 {
-	human_readable_cmd(cmd);
+	char	*str;
+
+	str = human_readable_cmd(cmd);
+	printf("%s\n", str);
+	free(str);
 	return (NULL);
 }

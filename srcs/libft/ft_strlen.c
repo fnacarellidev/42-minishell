c72c908857ef5b6a6736ecde31eca978b7a36a8f
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-22 17:05:59 -0300

    fix: treating case where received empty string

--- srcs/libft/ft_strlen.c
+++ srcs/libft/ft_strlen.c
@@ -6,17 +6,17 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 19:52:12 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/09/08 20:34:37 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/22 15:53:53 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 size_t	ft_strlen(const char *s)
 {
-	int	count;
+	int	i;
 
-	count = 0;
-	while (*(s + count))
-		count++;
-	return (count);
+	i = 0;
+	while (s && s[i])
+		i++;
+	return (i);
 }

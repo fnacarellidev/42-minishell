a1d44071c47c15241025afbb3bbcf4025de51013
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-22 16:58:36 -0300

    refactor: moved get_envp_list to separate file

---
+++ srcs/get_envp_list.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_envp_list.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 16:50:30 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/03/22 16:50:57 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../includes/minishell.h"
+
+static char	**get_matrix_with_key_value(char *env_variable)
+{
+	long int	adresses_diff;
+	char		**key_and_value;
+	char		*equal_occurence;
+
+	equal_occurence = ft_strchr(env_variable, '=');
+	adresses_diff = ft_labs(env_variable - equal_occurence);
+	key_and_value = ft_calloc(sizeof(char *), 3);
+	key_and_value[0] = ft_calloc(sizeof(char), adresses_diff + 2);
+	ft_strlcpy(key_and_value[0], env_variable, adresses_diff + 2);
+	key_and_value[0][adresses_diff] = '=';
+	key_and_value[1] = ft_strdup(++equal_occurence);
+	return (key_and_value);
+}
+
+t_node	*get_envp_list(char **envp)
+{
+	int		i;
+	char	**key_and_value;
+	t_node	*envp_list;
+	t_node	*new_node;
+
+	i = 0;
+	envp_list = NULL;
+	while (envp[i] != NULL)
+	{
+		key_and_value = get_matrix_with_key_value(envp[i]);
+		new_node = ft_lstnew(key_and_value[0], key_and_value[1]);
+		ft_free_matrix((void **)key_and_value);
+		ft_lstadd_back(&envp_list, new_node);
+		i++;
+	}
+	return (envp_list);
+}
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,34 +6,11 @@
 /*   By: fnacarel <fnacarel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 10:12:01 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/10 20:24:00 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/22 16:51:20 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 # include "../includes/minishell.h"
 #include "libft/libft.h"
-
-t_node	*get_envp_list(char **envp)
-{
-	int		i;
-	char	*tmp;
-	char	**split_key_value;
-	t_node	*envp_list;
-	t_node	*new_node;
-
-	i = 0;
-	envp_list = NULL;
-	while (envp[i] != NULL)
-	{
-		split_key_value = ft_split(envp[i], '=');
-		tmp = ft_strjoin(split_key_value[0], "=");
-		new_node = ft_lstnew(tmp, split_key_value[1]);
-		free(tmp);
-		ft_free_matrix((void **)split_key_value);
-		ft_lstadd_back(&envp_list, new_node);
-		i++;
-	}
-	return (envp_list);
-}
 
 void    ft_print_stack(t_node *stack)
 {

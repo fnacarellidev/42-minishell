4a3dface22fdffebcd8eacacd5bd976e4b835b38
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-22 19:03:29 -0300

    feat: put_spaces prototype added to header

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 10:17:56 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/22 16:52:48 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/22 18:51:53 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef MINISHELL_H
@@ -37,5 +37,8 @@ int		key_exists(t_node *envp_list, char *key);
 char    **lexer(char *cmd);
 
 t_node	*get_envp_list(char **envp);
+
+// Lexer
+int		put_spaces(char *str, int position_of_whole_string);
 
 #endif

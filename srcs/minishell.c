529dc3e0318ce98ff02dfa12c6dc4516b86b6036
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-23 15:54:40 -0300

    feat: receiving tokens and freeing it properly

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,10 +6,11 @@
 /*   By: fnacarel <fnacarel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 10:12:01 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/22 16:51:20 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/23 15:50:54 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/minishell.h"
+#include "libft/libft.h"
 
 void    ft_print_stack(t_node *stack)
 {
@@ -26,12 +27,15 @@ void    ft_print_stack(t_node *stack)
 
 int main(int argc, char **argv, char **envp)
 {
+	char	**tokens;
 	t_node	*envp_list;
+
 
 	if (argc && argv)
 	{}
 	envp_list = get_envp_list(envp);
-	lexer(argv[1]);
+	tokens = lexer(argv[1]);
+	ft_free_matrix((void**)tokens);
 	ft_free_list(&envp_list);
 	return (0);
 }

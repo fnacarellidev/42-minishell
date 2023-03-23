1dadc3b1c0a7012a1f8b3fe48abd2a56ec9bfcde
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-23 15:41:41 -0300

    feat: fix_tokens re-replaces the spaces that have been replaced right before

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 18:57:20 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/23 14:50:16 by revieira         ###   ########.fr       */
+/*   Updated: 2023/03/23 15:38:53 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
@@ -28,13 +28,31 @@ void    replace_between(char *str, char c1, char c2, char c3)
 	}
 }
 
+static void	fix_tokens(char **tokens)
+{
+	int	i;
+
+	i = 0;
+	while (tokens[i] != NULL)
+	{
+		replace_between(tokens[i], DEL_ASCII, SINGLE_QUOTE, ' ');
+		replace_between(tokens[i], DEL_ASCII, DOUBLE_QUOTE, ' ');
+		i++;
+	}
+}
+
 char	**lexer(char *cmd)
 {
 	char	*str;
+	char	**tokens;
 
 	str = human_readable_cmd(cmd);
 	erase_empty_quotes(str);
-	printf("%s\n", str);
+	replace_between(str, ' ', SINGLE_QUOTE, DEL_ASCII);
+	replace_between(str, ' ', DOUBLE_QUOTE, DEL_ASCII);
+	tokens = ft_split(str, ' ');
+	fix_tokens(tokens);
 	free(str);
+	ft_free_matrix((void**)tokens);
 	return (NULL);
 }

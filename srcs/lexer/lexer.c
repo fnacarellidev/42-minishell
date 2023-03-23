43be1c81176ea4a5c02636cb6260561200a47c5d
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-23 14:02:11 -0300

    feat: replace_between takes an string and removes the occurences of c1 between c2 for c3

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -36,6 +36,23 @@ void	erase_surrounder(char *str)
 	str[len_str - 2] = '\0';
 }
 
+void    replace_between(char *str, char c1, char c2, char c3)
+{
+	int	i;
+	int	in_between;
+
+	i = 0;
+	in_between = 0;
+	while (str[i])
+	{
+		if (str[i] == c2)
+			in_between = !in_between;
+		else if (in_between && str[i] == c1)
+			str[i] = c3;
+		i++;
+	}
+}
+
 char	**lexer(char *cmd)
 {
 	char	*str;

5ea601bb02077b2d557774ebe4ff2fb2d53b1eff
reinanmat <reinan266@gmail.com>
2023-03-23 14:56:27 -0300

    refactor: moved functions to erase_empty_quotes file

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,35 +6,10 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 18:57:20 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/23 13:56:24 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/23 14:50:16 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
-
-char	*get_empty_surrounder(char *str, char c1)
-{
-	while (*str)
-	{
-		if (*str == c1 && *(str + 1) == c1)
-			return (str);
-		str++;
-	}
-	return (NULL);
-}
-
-void	erase_surrounder(char *str)
-{
-	int	len_str;
-
-	len_str = ft_strlen(str);
-	if (str[2] == '\0')
-	{
-		str[0] = '\0';
-		return ;
-	}
-	ft_memmove(str, str + 2, len_str - 2);
-	str[len_str - 2] = '\0';
-}
 
 void    replace_between(char *str, char c1, char c2, char c3)
 {

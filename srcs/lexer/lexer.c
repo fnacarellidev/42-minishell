4830012a680a55ef6ed7708a2bc421e63c89509b
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-24 20:04:23 -0300

    refactor: no longer using replace_between_single_and_double wrapper func

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 18:57:20 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/23 15:50:34 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/24 20:03:24 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
@@ -18,7 +18,7 @@ static void	fix_tokens(char **tokens)
 	i = 0;
 	while (tokens[i] != NULL)
 	{
-		replace_between_single_and_double(tokens[i], DEL_ASCII, ' ');
+		replace_between(tokens[i], DEL_ASCII, ' ');
 		i++;
 	}
 }
@@ -30,7 +30,7 @@ char	**lexer(char *cmd)
 
 	str = human_readable_cmd(cmd);
 	erase_empty_quotes(str);
-	replace_between_single_and_double(str, ' ', DEL_ASCII);
+	replace_between(str, ' ', DEL_ASCII);
 	tokens = ft_split(str, ' ');
 	fix_tokens(tokens);
 	free(str);

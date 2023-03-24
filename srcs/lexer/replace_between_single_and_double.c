dd6a2e26d9dc715c3f51de595874d32c8975c1cb
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-24 20:00:29 -0300

    fix: was not considering cases where might have quotes inside quotes, now it's fixed

--- srcs/lexer/replace_between_single_and_double.c
+++ srcs/lexer/replace_between_single_and_double.c
@@ -6,24 +6,26 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 15:38:10 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/23 15:48:08 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/24 19:23:56 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
-static void	replace_between(char *str, char c1, char c2, char c3)
+void	replace_between(char *str, char c1, char c2)
 {
-	int	i;
-	int	in_between;
+	int		i;
+	char	quote;
 
 	i = 0;
-	in_between = 0;
+	quote = '\0';
 	while (str[i])
 	{
-		if (str[i] == c2)
-			in_between = !in_between;
-		else if (in_between && str[i] == c1)
-			str[i] = c3;
+		if ((str[i] == '\'' || str[i] == '\"') && quote == '\0')
+			quote = str[i];
+		else if (quote != '\0' && str[i] == c1)
+			str[i] = c2;
+		else if (str[i] == quote)
+			quote = '\0';
 		i++;
 	}
 }

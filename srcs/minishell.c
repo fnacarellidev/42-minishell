b3938aec866ce07edd507c476ba7f239cd4ee814
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-25 18:15:50 -0300

    feat: initializing minishell struct

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -39,6 +39,11 @@ void	ft_print_matrix(char **matrix)
 	}
 }
 
+void	init_minishell(void)
+{
+	minishell.status_code = 0;
+}
+
 int main(int argc, char **argv, char **envp)
 {
 	char	*cmd;
@@ -50,6 +55,7 @@ int main(int argc, char **argv, char **envp)
 	envp_list = get_envp_list(envp);
 	while (1)
 	{
+		init_minishell();
 		cmd = readline("$ ");
 		if (ft_strcmp(cmd, "exit") == 0)
 		{

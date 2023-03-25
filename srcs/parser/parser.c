d30bcb0f22a785d21df7b43e28d2f4b1b94a4b79
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-25 18:20:40 -0300

    feat: testing if we're validating the pipeline correctly

---
+++ srcs/parser/parser.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/25 15:54:18 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/03/25 17:27:40 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../../includes/minishell.h"
+
+int	parser(char **tokens)
+{
+	has_valid_syntax(tokens);
+	printf("status_code: %d\n", minishell.status_code);
+	return (0);
+}

572cfc785d991e50f03593cf6888a35c818df4d5
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-25 18:14:41 -0300

    fix: removing external quotes right now it's not a good idea

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,34 +6,10 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 18:57:20 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/24 21:16:49 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/25 18:14:00 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
-
-static void	remove_external_quotes(char *str)
-{
-	int		i;
-	char	quote;
-
-	i = 0;
-	quote = 0;
-	while (str[i])
-	{
-		if (is_quote(str[i]) && quote == '\0')
-		{
-			quote = str[i];
-			ft_memmove(str + i, str + i + 1, ft_strlen(str + i));
-		}
-		else if (is_quote(str[i]) && str[i] == quote)
-		{
-			quote = 0;
-			ft_memmove(str + i, str + i + 1, ft_strlen(str + i));
-		}
-		else
-			i++;
-	}
-}
 
 static void	fix_tokens(char **tokens)
 {
@@ -43,7 +19,6 @@ static void	fix_tokens(char **tokens)
 	while (tokens[i] != NULL)
 	{
 		replace_between(tokens[i], DEL_ASCII, ' ');
-		remove_external_quotes(tokens[i]);
 		i++;
 	}
 }

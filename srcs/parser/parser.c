36b4439c929320c42f951f00a3bb6bbb3f4ae792
reinanmat <reinan266@gmail.com>
2023-03-27 13:57:14 -0300

    fix: undoing last commit

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,14 +6,13 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 15:54:18 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/27 13:13:49 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/27 13:53:20 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
 int	parser(char **tokens)
 {
-	minishell.status_code = 0;
 	has_valid_syntax(tokens);
 	printf("status_code: %d\n", minishell.status_code);
 	return (0);

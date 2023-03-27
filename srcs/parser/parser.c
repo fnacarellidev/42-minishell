98af85827731f3b40b927ad83995b455342f8ae0
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-27 18:45:42 -0300

    feat: parser
    prints error message if needed, according to the value received by get_syntax_error_idx, if returned -2 it means there's nothing wrong, if returns something else, it's the index of the token that has something wrong with it

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,14 +6,26 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 15:54:18 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/27 13:53:20 by revieira         ###   ########.fr       */
+/*   Updated: 2023/03/27 18:21:18 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
 int	parser(char **tokens)
 {
-	has_valid_syntax(tokens);
-	printf("status_code: %d\n", minishell.status_code);
+	int		idx_err;
+	char	*err_token;
+
+	idx_err = get_syntax_error_idx(tokens);
+	if (idx_err != -2)
+	{
+		if (tokens[idx_err + 1] == NULL)
+			err_token = ft_strdup("newline");
+		else
+			err_token = ft_strdup(tokens[idx_err + 1]);
+		printf("bash: syntax error near unexpected token `%s'\n", err_token);
+		free(err_token);
+	}
+	/* printf("status_code: %d\nerr at idx: %d\n", minishell.status_code, minishell.token_syntax_err_idx); */
 	return (0);
 }

3ee1fbd9f4dd57a5b14ae474e271d64026e2e969
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-28 13:11:50 -0300

    style: norminette fixed on has_valid_syntax.c

--- srcs/parser/has_valid_syntax.c
+++ srcs/parser/has_valid_syntax.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 16:07:54 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/27 18:42:21 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/28 13:10:47 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
@@ -33,7 +33,7 @@ static int	syntax_error_on_pipe(char **tokens, int pos)
 	if (pos == 0)
 		return (-2);
 	if (tokens[pos + 1] == NULL
-			|| is_preceeded_or_followed_by_metachar(tokens, pos))
+		|| is_preceeded_or_followed_by_metachar(tokens, pos))
 	{
 		minishell.status_code = 2;
 		return (1);
@@ -74,7 +74,8 @@ int	get_syntax_error_idx(char **tokens)
 				return (-1);
 			return (i);
 		}
-		else if (is_meta_char(*tokens[i]) && syntax_error_on_redirect(tokens[i + 1]))
+		else if (is_meta_char(*tokens[i])
+			&& syntax_error_on_redirect(tokens[i + 1]))
 			return (i);
 		else if (has_unclosed_quote(tokens[i]))
 			return (i);

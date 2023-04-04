49e24e3aa246eebeb5063905b576f8827ea61778
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-04 17:02:07 -0300

    feat: replacing set1 by set2 instead of a single char

--- srcs/lexer/replace_between_single_and_double.c
+++ srcs/lexer/replace_between_single_and_double.c
@@ -6,14 +6,25 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 15:38:10 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/03/24 21:01:19 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/04/04 16:39:09 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
-void	replace_between(char *str, char c1, char c2)
+int	get_pos(char c, char *set)
+{
+	int	i;
+
+	i = 0;
+	while (c != set[i])
+		i++;
+	return (i);
+}
+
+void	replace_between(char *str, char *set1, char *set2)
 {
 	int		i;
+	int		corresponding_position;
 	char	quote;
 
 	i = 0;
@@ -22,8 +33,11 @@ void	replace_between(char *str, char c1, char c2)
 	{
 		if (is_quote(str[i]) && quote == '\0')
 			quote = str[i];
-		else if (quote != '\0' && str[i] == c1)
-			str[i] = c2;
+		else if (quote != '\0' && ft_strchr(set1, str[i]))
+		{
+			corresponding_position = get_pos(str[i], set1);
+			str[i] = set2[corresponding_position];
+		}
 		else if (str[i] == quote)
 			quote = '\0';
 		i++;

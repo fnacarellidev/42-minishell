5acfa4b82e5bb90ea79586ddb06a6c1dce491c8b
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-06 19:05:03 -0300

    feat: erase_empty_quotes
    erases empty quotes in tokens and removes external quotes too

--- srcs/parser/erase_empty_quotes.c
+++ srcs/parser/erase_empty_quotes.c
@@ -43,3 +43,22 @@ void	clear_subtokens(char **subtokens)
 		i++;
 	}
 }
+
+void	erase_empty_quotes(char **tokens)
+{
+	int		i;
+	char	quoted;
+	char	**subtokens;
+
+	i = 0;
+	quoted = 0;
+	while (tokens[i] != NULL)
+	{
+		subtokens = get_subtokens(tokens[i], 0);
+		clear_subtokens(subtokens);
+		free(tokens[i]);
+		tokens[i] = concat_subtokens(subtokens);
+		free(subtokens);
+		i++;
+	}
+}

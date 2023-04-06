894888d2119fc54b04739913d052b96649f81e92
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-06 19:03:33 -0300

    feat: erase_external_quotes
    erases surrounding quotes for given string

--- srcs/parser/erase_empty_quotes.c
+++ srcs/parser/erase_empty_quotes.c
@@ -17,3 +17,14 @@ int		is_empty_quote(char *str)
 		return (1);
 	return (0);
 }
+
+void	erase_external_quotes(char *str)
+{
+	int	last_pos;
+	int	str_len;
+
+	str_len = ft_strlen(str);
+	ft_memmove(str, str + 1, str_len);
+	last_pos = str_len - 2;
+	ft_memset(str + last_pos, 0, 1);
+}

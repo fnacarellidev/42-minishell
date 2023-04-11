4d65e038a1798d890cc77e7c272903195f7155c1
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-11 17:40:30 -0300

    refactor: init_redirects changed completely
    instead of just searching for the last redirect token, now we iterate in every token and if we find any redirect we do what we must do related to that redirect

--- srcs/executor/init_redirects.c
+++ srcs/executor/init_redirects.c
@@ -6,34 +6,26 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 15:06:37 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/10 18:24:01 by revieira         ###   ########.fr       */
+/*   Updated: 2023/04/11 17:12:33 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
-static char	*get_filename_of_given_token(char **tokens, char redirect)
-{
-	int	last_pos;
-
-	last_pos = ft_count_matrix((void **)tokens) - 1;
-	while (last_pos >= 0)
-	{
-		if (tokens[last_pos][0] == redirect)
-			return (tokens[last_pos + 1]);
-		last_pos--;
-	}
-	return (NULL);
-}
-
 void	init_redirects(char **tokens)
 {
-	char	*input_filename;
-	char	*output_filename;
+	int	i;
 
-	input_filename = get_filename_of_given_token(tokens, '<');
-	output_filename = get_filename_of_given_token(tokens, '>');
-	if (input_filename)
-		g_minishell.pipeline.input_fd = open(input_filename, O_RDONLY);
-	if (output_filename)
-		g_minishell.pipeline.output_fd = open(output_filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	i = 0;
+	while (tokens[i])
+	{
+		if (ft_strcmp("<<", tokens[i]) == 0)
+			swap_stream_fd("input", get_heredoc_fd(tokens[++i]));
+		else if (ft_strcmp("<", tokens[i]) == 0)
+			swap_stream_fd("input", open(tokens[++i], O_RDONLY));
+		else if (ft_strcmp(">", tokens[i]) == 0)
+			swap_stream_fd("output", open(tokens[++i], O_WRONLY));
+		else if (ft_strcmp(">>", tokens[i]) == 0)
+			swap_stream_fd("output", open(tokens[++i], O_WRONLY | O_APPEND));
+		i++;
+	}
 }

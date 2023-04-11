c4ec34f7659a815b6cacbc55269d794cfea501d7
reinanmat <reinan266@gmail.com>
2023-04-11 14:03:54 -0300

    fix: making comparison with ft_strcmp, to avoid error with append and heredoc

--- srcs/executor/validate_redirects.c
+++ srcs/executor/validate_redirects.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 15:18:06 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/10 16:37:15 by revieira         ###   ########.fr       */
+/*   Updated: 2023/04/11 11:51:41 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
@@ -59,9 +59,9 @@ int	validate_redirects(char **tokens)
 	ret = 0;
 	while (tokens[i] && ret == 0)
 	{
-		if (tokens[i][0] == '<')
+		if (ft_strcmp(tokens[i], "<") == 0)
 			ret = validate_input_redirects(tokens[i + 1]);
-		else if (tokens[i][0] == '>')
+		else if (ft_strcmp(tokens[i], ">") == 0)
 			ret = validate_output_redirects(tokens[i + 1]);
 		i++;
 	}

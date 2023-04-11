f6083e3e1ed01ea2081a70f77b3b72e7040c7f1e
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-11 19:02:58 -0300

    feat: number_of_commands
    number of commands will return the number of pipes on tokens plus 1, which will be the accurate number of commands in most cases

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -6,11 +6,24 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 16:38:16 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/11 17:27:31 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/04/11 19:01:06 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
+static int	number_of_commands(char **tokens_without_redirects)
+{
+	int	i;
+	int	j;
+
+	while (tokens_without_redirects[i])
+	{
+		if (ft_strcmp("|", tokens_without_redirects[i]) == 0)
+			j++;
+		i++;
+	}
+	return (j + 1);
+}
 void	executor(char **tokens)
 {
 	if (validate_redirects(tokens) == 1)

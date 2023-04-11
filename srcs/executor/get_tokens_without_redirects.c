0a8ef330a438f60c500fb25fa5e2fb6d458c4fbd
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-11 19:04:16 -0300

    feat: exclude_redirects
    will return the number of position to alloc, not considering redirects and it's filenames

---
+++ srcs/executor/get_tokens_without_redirects.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_tokens_without_redirects.c                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/11 18:36:32 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/04/11 18:52:37 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../../includes/minishell.h"
+
+static int	exclude_redirects(char **tokens)
+{
+	int	i;
+	int	mem_to_alloc;
+
+	i = 0;
+	mem_to_alloc = 0;
+	while (tokens[i])
+	{
+		if (is_redirect(tokens[i]))
+			i++;
+		else
+			mem_to_alloc++;
+		i++;
+	}
+	return (mem_to_alloc);
+}

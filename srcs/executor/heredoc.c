181a980720bbbeaecdd309a2200cb4b6b0cdd868
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-12 17:53:08 -0300

    feat: validate_line
    validate line was basically created to solve our function exceeding 25 lines and now heredoc expands variables

--- srcs/executor/heredoc.c
+++ srcs/executor/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 14:08:24 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/11 17:33:11 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/04/12 17:49:53 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
@@ -15,6 +15,17 @@ static void	here_doc_err(char *arg)
 {
 	printf("bash: warning: here-document delimited by end of file" \
 			" (wanted `%s`)\n", arg);
+}
+
+static char	*validate_line(void)
+{
+	char	*line;
+
+	line = get_next_line(0);
+	if (!line)
+		return (NULL);
+	expand_vars(&line);
+	return (line);
 }
 
 int	get_heredoc_fd(char *arg)
@@ -28,7 +39,7 @@ int	get_heredoc_fd(char *arg)
 	while (1)
 	{
 		write(1, "> ", 2);
-		line = get_next_line(0);
+		line = validate_line();
 		if (!line || !ft_strcmp(line, delimiter))
 		{
 			if (line)

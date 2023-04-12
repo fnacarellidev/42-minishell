3ca20b6d9af5769ef57908669d41aeea2e906898
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-12 15:19:20 -0300

    feat: count_commands
    returns the number of occurrences of pipe token plus 1

---
+++ srcs/executor/init_pipeline.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_pipeline.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/12 13:14:37 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/04/12 15:11:18 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../../includes/minishell.h"
+
+static int	count_commands(char **tokens)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (tokens[i])
+	{
+		if (ft_strcmp("|", tokens[i]) == 0)
+			j++;
+		i++;
+	}
+	return (j + 1);
+}

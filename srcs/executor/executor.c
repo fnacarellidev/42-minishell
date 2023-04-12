88edaa5466173ea641dc584ebc46b7cd697b7ecf
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-12 15:23:57 -0300

    refactor: functions got moved to different file

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -6,46 +6,18 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 16:38:16 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/11 19:10:24 by revieira         ###   ########.fr       */
+/*   Updated: 2023/04/12 13:52:37 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
-static int	number_of_commands(char **tokens_without_redirects)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (tokens_without_redirects[i])
-	{
-		if (ft_strcmp("|", tokens_without_redirects[i]) == 0)
-			j++;
-		i++;
-	}
-	return (j + 1);
-}
-
-static void	init_pipeline(char **tokens)
-{
-	int	mem;
-
-	g_minishell.pipeline.number_of_cmds = number_of_commands(tokens);
-	mem = g_minishell.pipeline.number_of_cmds;
-	g_minishell.pipeline.commands = ft_calloc(sizeof(t_command), mem + 1);
-}
-
 void	executor(char **tokens)
 {
-	char	**new_tokens;
-
 	if (validate_redirects(tokens) == 1)
 	{
 		g_minishell.status_code = 1;
 		return ;
 	}
 	init_redirects(tokens);
-	new_tokens = get_tokens_without_redirects(tokens);
-	init_pipeline(new_tokens);
+	init_pipeline(tokens);
 }

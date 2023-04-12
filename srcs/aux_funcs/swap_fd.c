2f1fde932dc1d2a53aac8aebebc2579a3762dd62
reinanmat <reinan266@gmail.com>
2023-04-12 16:52:57 -0300

    refactor: now swap_stream_fd receives command
    instead of changing directly in the struct g_minishell

--- srcs/aux_funcs/swap_fd.c
+++ srcs/aux_funcs/swap_fd.c
@@ -6,29 +6,29 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 16:52:43 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/04/11 17:05:18 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/04/12 16:10:01 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
-static void	swap_fd_input(int new_fd)
+static void	swap_fd_input(t_command *command, int new_fd)
 {
-	if (g_minishell.pipeline.input_fd != 0)
-		close(g_minishell.pipeline.input_fd);
-	g_minishell.pipeline.input_fd = new_fd;
+	if (command->input_fd != 0)
+		close(command->input_fd);
+	command->input_fd = new_fd;
 }
 
-static void	swap_fd_output(int new_fd)
+static void	swap_fd_output(t_command *command, int new_fd)
 {
-	if (g_minishell.pipeline.output_fd != 1)
-		close(g_minishell.pipeline.output_fd);
-	g_minishell.pipeline.output_fd = new_fd;
+	if (command->output_fd != 1)
+		close(command->output_fd);
+	command->output_fd = new_fd;
 }
 
-void	swap_stream_fd(char *stream, int new_fd)
+void	swap_stream_fd(char *stream, t_command *command, int new_fd)
 {
 	if (ft_strcmp(stream, "input") == 0)
-		swap_fd_input(new_fd);
+		swap_fd_input(command, new_fd);
 	else
-		swap_fd_output(new_fd);
+		swap_fd_output(command, new_fd);
 }

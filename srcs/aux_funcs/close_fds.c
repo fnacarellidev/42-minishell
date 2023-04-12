1874ebde878386467193ab5be0c9000ae6668f0b
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-12 15:16:59 -0300

    refactor: changed close_fds
    due to changes on how the struct is implemented I needed to make close_fds iterative since each input and output fd will be stored in each command created

--- srcs/aux_funcs/close_fds.c
+++ srcs/aux_funcs/close_fds.c
@@ -6,15 +6,24 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 17:15:12 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/04/11 17:16:04 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/04/12 15:06:02 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
 void	close_fds(void)
 {
-	if (g_minishell.pipeline.input_fd != 0)
-		close(g_minishell.pipeline.input_fd);
-	if (g_minishell.pipeline.output_fd != 1)
-		close(g_minishell.pipeline.output_fd);
+	int	i;
+	int	commands_size;
+
+	i = 0;
+	commands_size = g_minishell.number_of_cmds;
+	while (i < commands_size)
+	{
+		if (g_minishell.commands[i].input_fd != 0)
+			close(g_minishell.commands[i].input_fd);
+		if (g_minishell.commands[i].output_fd != 1)
+			close(g_minishell.commands[i].output_fd);
+		i++;
+	}
 }

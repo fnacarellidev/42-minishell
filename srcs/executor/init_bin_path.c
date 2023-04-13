61e343632fe339fd5c18a172ad90668e8a08ceb4
reinanmat <reinan266@gmail.com>
2023-04-13 18:50:25 -0300

    feat: get_path_dirs
    returns a pointer to the directories of path

---
+++ srcs/executor/init_bin_path.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_bin_path.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/12 18:42:12 by revieira          #+#    #+#             */
+/*   Updated: 2023/04/13 18:35:07 by revieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../../includes/minishell.h"
+
+static char	**get_path_dirs(void)
+{
+	int		i;
+	char	*path;
+	char	**path_dirs;
+
+	i = 0;
+	path = get_key_value(g_minishell.envp, "PATH");
+	path_dirs = ft_split_old(path, ':');
+	while (path_dirs[i])
+	{
+		append(&path_dirs[i], ft_strdup("/"));
+		i++;
+	}
+	return (path_dirs);
+}
+
+static char	*get_bin_path(t_command *command)
+{
+	int		i;
+	char	*bin;
+	char	**path_dirs;
+
+	i = 0;
+	path_dirs = get_path_dirs();
+	while (path_dirs[i])
+	{
+		bin = ft_strjoin(path_dirs[i], command->args[0]);
+		if (access(bin, F_OK | X_OK) == 0)
+		{
+			ft_free_matrix((void **)path_dirs);
+			return (bin);
+		}
+		else if (access(bin, F_OK) == 0 && access(bin, X_OK) == -1)
+		{
+			command->permission_denied = 1;
+			ft_free(bin);
+			break ;
+		}
+		ft_free(bin);
+		i++;
+	}
+	ft_free_matrix((void **)path_dirs);
+	return (NULL);
+}
+

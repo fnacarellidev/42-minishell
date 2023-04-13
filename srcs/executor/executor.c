7daf97b2f8058af34d1be3b2a52fa09349f337c1
reinanmat <reinan266@gmail.com>
2023-04-13 18:45:39 -0300

    feat: added print_struct

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -6,10 +6,24 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 16:38:16 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/12 19:04:23 by revieira         ###   ########.fr       */
+/*   Updated: 2023/04/13 18:35:39 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
+
+void	print_struct(void)
+{
+	int	i;
+	int	args;
+
+	i = 0;
+	args = g_minishell.number_of_cmds;
+	while (i < args)
+	{
+		ft_print_matrix(g_minishell.commands[i].args);
+		i++;
+	}
+}
 
 void	executor(char **tokens)
 {

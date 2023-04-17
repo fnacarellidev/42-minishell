d97e8b03852f8e39dcd082e39e394cd17d71f910
reinanmat <reinan266@gmail.com>
2023-04-17 15:53:31 -0300

    feat: init_executor
    initialize struct commands

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 16:38:16 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/13 18:35:39 by revieira         ###   ########.fr       */
+/*   Updated: 2023/04/17 15:23:42 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
@@ -25,16 +25,45 @@ void	print_struct(void)
 	}
 }
 
+static int	count_commands(char **tokens)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (tokens[i])
+	{
+		if (ft_strcmp("|", tokens[i]) == 0)
+			j++;
+		i++;
+	}
+	return (j + 1);
+}
+
+void	init_executor(char **tokens)
+{
+	int	number_of_cmds;
+
+	g_minishell.number_of_cmds = count_commands(tokens);
+	number_of_cmds = g_minishell.number_of_cmds;
+	g_minishell.commands = ft_calloc(sizeof(t_command), number_of_cmds + 1);
+	init_commands(tokens, 0);
+	init_redirects();
+	remove_redirects();
+	remove_quotes();
+	init_bin_path();
+}
+
 void	executor(char **tokens)
 {
+	int	pid;
+
 	if (validate_redirects(tokens) == 1)
 	{
 		g_minishell.status_code = 1;
 		return ;
 	}
-	init_pipeline(tokens);
-	init_redirects();
-	remove_redirects();
-	remove_quotes();
-	init_bin_path();
+	init_executor(tokens);
+	pid = fork();
 }

009559429347aa6572c1ef7a7d38db1a070d971d
reinanmat <reinan266@gmail.com>
2023-04-17 16:04:37 -0300

    feat: clean g_minishell.envp

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -23,7 +23,8 @@ void	init_minishell(char **envp)
 void	die(void)
 {
 	rl_clear_history();
-	ft_free_list(&g_minishell.envp);
+	ft_free_list(&g_minishell.envp_list);
+	ft_free_matrix((void **)g_minishell.envp);
 	close_fds();
 	unlink(TMPFILE);
 	write(1, "exit\n", 5);

46e0a15f7d6a4d4b1a452d1d0ec665200b4ff700
reinanmat <reinan266@gmail.com>
2023-04-19 17:15:40 -0300

    feat: added macro EISDIR

--- includes/errors.h
+++ includes/errors.h
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 16:27:54 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/10 16:35:14 by revieira         ###   ########.fr       */
+/*   Updated: 2023/04/19 16:29:12 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef ERRORS_H
@@ -15,5 +15,6 @@
 # define ENOENT 2
 # define EACCES 13
 # define ENAMETOOLONG 36
+# define EISDIR 126
 
 #endif

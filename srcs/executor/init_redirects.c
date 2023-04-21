2ec36a1d4633c773f60e0213e15be9b9e736e39b
reinanmat <reinan266@gmail.com>
2023-04-21 13:54:03 -0300

    refactor: reworking fill_fds
    now this function stops the execution of init_redirects if there is an error in heredoc

--- srcs/executor/init_redirects.c
+++ srcs/executor/init_redirects.c
@@ -6,31 +6,36 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 15:06:37 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/12 16:21:45 by revieira         ###   ########.fr       */
+/*   Updated: 2023/04/20 19:19:02 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
-static void	fill_fds(t_command *command)
+static void	fill_fds(t_command *cmd)
 {
 	int		i;
-	char	**args;
+	int		heredoc_status;
 
 	i = 0;
-	args = command->args;
-	command->input_fd = 0;
-	command->output_fd = 1;
-	while (args[i])
+	heredoc_status = 0;
+	cmd->input_fd = 0;
+	cmd->output_fd = 1;
+	while (cmd->args[i])
 	{
-		if (ft_strcmp("<<", args[i]) == 0)
-			swap_stream_fd("input", command, get_heredoc_fd(args[++i]));
-		else if (ft_strcmp("<", args[i]) == 0)
-			swap_stream_fd("input", command, open(args[++i], O_RDONLY));
-		else if (ft_strcmp(">", args[i]) == 0)
-			swap_stream_fd("output", command, open(args[++i], O_WRONLY));
-		else if (ft_strcmp(">>", args[i]) == 0)
-			swap_stream_fd("output", command, open(args[++i], \
+		if (ft_strcmp("<<", cmd->args[i]) == 0)
+			heredoc_status = heredoc(cmd, cmd->args[++i]);
+		else if (ft_strcmp("<", cmd->args[i]) == 0)
+			swap_stream_fd("input", cmd, open(cmd->args[++i], O_RDONLY));
+		else if (ft_strcmp(">", cmd->args[i]) == 0)
+			swap_stream_fd("output", cmd, open(cmd->args[++i], O_WRONLY));
+		else if (ft_strcmp(">>", cmd->args[i]) == 0)
+			swap_stream_fd("output", cmd, open(cmd->args[++i], \
 				O_WRONLY | O_APPEND));
+		if (heredoc_status != 0)
+		{
+			g_minishell.heredoc.heredoc_exited = 1;
+			return ;
+		}
 		i++;
 	}
 }
@@ -42,7 +47,7 @@ void	init_redirects(void)
 
 	i = 0;
 	args = g_minishell.number_of_cmds;
-	while (i < args)
+	while (i < args && !g_minishell.heredoc.heredoc_exited)
 	{
 		fill_fds(&g_minishell.commands[i]);
 		i++;

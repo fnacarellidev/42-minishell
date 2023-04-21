aa45938795b05c176269d6b820d32ff47521a066
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-21 19:07:13 -0300

    fix: open was swapped, fixed it

--- srcs/aux_funcs/test_filename.c
+++ srcs/aux_funcs/test_filename.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 17:15:31 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/04/21 17:15:48 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/04/21 17:36:34 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
@@ -16,11 +16,11 @@ int	test_filename(char *filename, char *redirect)
 	int	fd;
 
 	if (ft_strcmp(redirect, ">") == 0)
-		fd = open(filename, O_RDONLY);
+		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	else if (ft_strcmp(redirect, ">>") == 0)
 		fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0644);
 	else
-		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		fd = open(filename, O_RDONLY);
 	if (fd != -1)
 		close(fd);
 	return (fd);

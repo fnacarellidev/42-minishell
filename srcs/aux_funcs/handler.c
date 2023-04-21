7ffce2a854e8606e3b3ef320b4a7d609fda99346
reinanmat <reinan266@gmail.com>
2023-04-21 13:48:40 -0300

    refactor: checking if the process is not inside the heredoc fork

--- srcs/aux_funcs/handler.c
+++ srcs/aux_funcs/handler.c
@@ -6,14 +6,14 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 16:56:08 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/12 16:56:38 by revieira         ###   ########.fr       */
+/*   Updated: 2023/04/21 13:37:59 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
 void	handler(int signal)
 {
-	if (signal == SIGINT)
+	if (signal == SIGINT && g_minishell.heredoc.on_heredoc != 1)
 	{
 		write(1, "\n", 1);
 		rl_on_new_line();

bfe627aa37e16c0fe10a7a429c64e05586f1aabb
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-21 14:38:13 -0300

    fix: fix prompt bug when ctrl+c on loop
    when ctrl+c was pressed inside a loop it would print the prompt prefix twice, now it's not doing that

--- srcs/aux_funcs/handler.c
+++ srcs/aux_funcs/handler.c
@@ -6,18 +6,20 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 16:56:08 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/21 13:37:59 by revieira         ###   ########.fr       */
+/*   Updated: 2023/04/21 14:28:35 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
 void	handler(int signal)
 {
-	if (signal == SIGINT && g_minishell.heredoc.on_heredoc != 1)
+	if (signal == SIGINT && !g_minishell.on_fork)
 	{
 		write(1, "\n", 1);
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
 	}
+	else if (signal == SIGINT && g_minishell.on_fork)
+		write(1, "\n", 1);
 }

9fdb9120b37408be996d5616c935cf6a0b38235b
reinanmat <reinan266@gmail.com>
2023-04-21 13:26:40 -0300

    refactor: die_child
    now it works for both the executor fork and the heredoc fork

--- srcs/aux_funcs/die_child.c
+++ srcs/aux_funcs/die_child.c
@@ -6,17 +6,17 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 17:50:27 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/04/19 18:12:18 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/04/21 11:58:59 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
-void	die_child(t_command cmd)
+void	die_child(int heredoc, int exit_code)
 {
-	int	exit_code;
-
-	exit_code = cmd.error;
-	close_fds_in_child();
+	if (heredoc)
+		close(g_minishell.heredoc.fd);
+	else
+		close_fds_in_child();
 	ft_free_commands();
 	ft_free_matrix((void **)g_minishell.envp);
 	ft_free_list(&g_minishell.envp_list);

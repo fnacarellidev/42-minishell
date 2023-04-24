b9fc5b655fc2cd4788995395554687e3399a4015
reinanmat <reinan266@gmail.com>
2023-04-24 13:27:29 -0300

    refactor: removing remove_quotes from parser
    now remove_quotes in executor

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,43 +6,10 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 15:54:18 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/04/21 18:30:53 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/04/24 13:24:56 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
-
-static int	is_empty_quote(char *str)
-{
-	if (*str == str[1])
-		return (1);
-	return (0);
-}
-
-static void	erase_external_quotes(char *str)
-{
-	int	last_pos;
-	int	str_len;
-
-	str_len = ft_strlen(str);
-	ft_memmove(str, str + 1, str_len);
-	last_pos = str_len - 2;
-	ft_memset(str + last_pos, 0, 1);
-}
-
-static void	clear_subtokens(char **subtokens)
-{
-	int	i;
-
-	i = 0;
-	while (subtokens[i])
-	{
-		if (is_quote(*subtokens[i]) && is_empty_quote(subtokens[i]))
-			ft_memset(subtokens[i], 0, 2);
-		else if (is_quote(*subtokens[i]))
-			erase_external_quotes(subtokens[i]);
-		i++;
-	}
-}
 
 static int	print_invalid_syntax(int idx_err, char **tokens)
 {
@@ -56,27 +23,6 @@ static int	print_invalid_syntax(int idx_err, char **tokens)
 	g_minishell.status_code = 2;
 	free(err_token);
 	return (1);
-}
-
-void	remove_quotes2(char **tokens)
-{
-	int		i;
-	char	**subtokens;
-
-	i = 0;
-	while (tokens[i])
-	{
-		if (ft_strcmp(tokens[i], ">") || ft_strcmp(tokens[i], "<") \
-				|| ft_strcmp(tokens[i], ">>") || ft_strcmp(tokens[i], "<<"))
-		{
-			subtokens = get_subtokens(tokens[i], 0);
-			clear_subtokens(subtokens);
-			free(tokens[i]);
-			tokens[i] = concat_subtokens(subtokens);
-			free(subtokens);
-		}
-		i++;
-	}
 }
 
 int	parser(char **tokens)
@@ -93,7 +39,6 @@ int	parser(char **tokens)
 		expand_token(tokens + i);
 		i++;
 	}
-	remove_quotes2(tokens);
 	print_possible_redirect_error(tokens);
 	return (0);
 }

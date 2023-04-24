1a9e97d499e9897f77f9370b64e5d6a26cf4166f
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-24 19:52:36 -0300

    refactor: ft_pwd was ugly af

--- srcs/builtins/ft_pwd.c
+++ srcs/builtins/ft_pwd.c
@@ -6,17 +6,20 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 16:37:50 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/04/24 16:37:57 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/04/24 18:29:26 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
 int	ft_pwd(t_command cmd)
 {
+	char	*cwd;
+
+	cwd = getcwd(NULL, 0);
 	if (cmd.args[0])
-	{
-		printf("%s\n", getcwd(NULL, 0));
+		printf("%s\n", cwd);
+	ft_free(cwd);
+	if (g_minishell.on_fork)
 		exit(0);
-	}
-	exit(-42);
+	return (0);
 }

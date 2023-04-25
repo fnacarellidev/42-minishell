f81a053434fe7df488510c8c5623c9ca3e3f9a4e
reinanmat <reinan266@gmail.com>
2023-04-25 14:05:10 -0300

    fix: excessive line breaks in heredoc
    checking if on_fork variable is 1, 1 for executor and 2 for heredoc

--- srcs/aux_funcs/handler.c
+++ srcs/aux_funcs/handler.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 16:56:08 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/21 14:28:35 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/04/25 14:00:56 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
@@ -20,6 +20,6 @@ void	handler(int signal)
 		rl_replace_line("", 0);
 		rl_redisplay();
 	}
-	else if (signal == SIGINT && g_minishell.on_fork)
+	else if (signal == SIGINT && g_minishell.on_fork == 1)
 		write(1, "\n", 1);
 }

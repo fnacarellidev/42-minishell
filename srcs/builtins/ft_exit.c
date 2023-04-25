d45aaf1c4a0922e192eacf71c9e53fdd33d8f139
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-25 15:44:40 -0300

    feat: ft_exit implemented, passed every test

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -6,14 +6,74 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 22:09:36 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/04/24 22:14:46 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/04/25 15:38:42 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
+static void	free_and_print(void)
+{
+	close_fds();
+	ft_free_commands();
+	ft_free_matrix((void **)g_minishell.envp);
+	ft_free_list(&g_minishell.envp_list);
+	rl_clear_history();
+	printf("exit\n");
+}
+
+static int	has_non_numeric_char(char *first_arg)
+{
+	while (first_arg && *first_arg)
+	{
+		if (!ft_isdigit(*first_arg) && *first_arg != '-' && *first_arg != '+')
+			return (1);
+		first_arg++;
+	}
+	return (0);
+}
+
+static int	out_of_range(char *nptr)
+{
+	int			nptr_len;
+	long int	nbr;
+
+	nptr_len = ft_strlen(nptr);
+	nbr = ft_latoi(nptr);
+	if (nptr_len > 1 && nbr == 0)
+		return (1);
+	return (0);
+}
+
+static int	get_exit_code(t_command cmd)
+{
+	if (out_of_range(cmd.args[1]))
+	{
+		ft_printf(STDERR_FILENO, \
+				"bash: exit: %s: numeric argument required\n", cmd.args[1]);
+		return (2);
+	}
+	else
+		return (ft_latoi(cmd.args[1]));
+}
+
 int	ft_exit(t_command cmd)
 {
-	if (cmd.args[1])
-		exit(ft_atoi(cmd.args[1]));
-	exit (0);
+	long int	exit_code;
+
+	exit_code = 0;
+	if (has_non_numeric_char(cmd.args[1]))
+	{
+		ft_printf(STDERR_FILENO, \
+			"bash: exit: %s: numeric argument required\n", cmd.args[1]);
+		exit_code = 2;
+	}
+	else if (cmd.number_of_args > 2)
+	{
+		ft_printf(STDERR_FILENO, "bash: exit: too many arguments\n");
+		exit_code = 1;
+	}
+	else
+		exit_code = get_exit_code(cmd);
+	free_and_print();
+	exit(exit_code);
 }

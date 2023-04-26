dde0d802ef9f5742d5cdf53a523ee26ad5fa91af
reinanmat <reinan266@gmail.com>
2023-04-26 15:13:23 -0300

    fix: fixing error of fds that were not being closed in the fork

--- srcs/builtins/run_builtin.c
+++ srcs/builtins/run_builtin.c
@@ -6,7 +6,7 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 11:56:43 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/26 13:23:27 by revieira         ###   ########.fr       */
+/*   Updated: 2023/04/26 15:09:49 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
@@ -36,7 +36,19 @@ void	run_builtin(t_command cmd, int (*builtin)(t_command cmd))
 	stdout_backup = dup(STDOUT_FILENO);
 	handle_input(cmd);
 	handle_output(cmd);
-	g_minishell.status_code = builtin(cmd);
-	dup2(stdin_backup, STDIN_FILENO);
-	dup2(stdout_backup, STDOUT_FILENO);
+	if (g_minishell.on_fork)
+	{
+		close_fds_in_child();
+		close(stdin_backup);
+		close(stdout_backup);
+		builtin(cmd);
+	}
+	else
+	{
+		g_minishell.status_code = builtin(cmd);
+		dup2(stdin_backup, STDIN_FILENO);
+		dup2(stdout_backup, STDOUT_FILENO);
+		close(stdin_backup);
+		close(stdout_backup);
+	}
 }

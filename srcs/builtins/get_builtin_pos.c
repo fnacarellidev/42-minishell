aaa28bf55d889e0dfa8cc6538f1c65215e88a139
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-26 11:13:49 -0300

    fix: get_builtin_pos now have null parameter treatment

--- srcs/builtins/get_builtin_pos.c
+++ srcs/builtins/get_builtin_pos.c
@@ -6,13 +6,15 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 15:27:02 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/04/24 17:47:08 by revieira         ###   ########.fr       */
+/*   Updated: 2023/04/26 11:02:02 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
 
 int	get_builtin_pos(char *str)
 {
+	if (!str)
+		return (-1);
 	if (ft_strcmp(str, "echo") == 0)
 		return (0);
 	else if (ft_strcmp(str, "cd") == 0)

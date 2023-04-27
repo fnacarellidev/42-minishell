6728bf821198b0fe13369ac4df1799110391adac
reinanmat <reinan266@gmail.com>
2023-04-27 20:03:08 -0300

    fix: treating pointer to null

--- srcs/libft/ft_free_matrix_size_n.c
+++ srcs/libft/ft_free_matrix_size_n.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 16:15:13 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/12/21 16:16:34 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/04/27 19:45:48 by revieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -18,7 +18,8 @@ void	ft_free_matrix_size_n(void **matrix, int size)
 	i = 0;
 	while (i < size)
 	{
-		free(matrix[i]);
+		if (matrix[i])
+			free(matrix[i]);
 		i++;
 	}
 	free(matrix);

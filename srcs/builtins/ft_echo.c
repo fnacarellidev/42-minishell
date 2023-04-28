bcd72461bfea0c4d34eb1f7b4de12ae59e5f4383
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-04-28 13:11:11 -0300

    fix: echo -nnnnnnnnnnnnnn works

--- srcs/builtins/ft_echo.c
+++ srcs/builtins/ft_echo.c
@@ -6,17 +6,33 @@
 /*   By: revieira <revieira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 17:20:08 by revieira          #+#    #+#             */
-/*   Updated: 2023/04/28 10:16:07 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/04/28 12:55:54 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/minishell.h"
+
+static int	valid_dash_n(char *dash_n)
+{
+	int	i;
+
+	i = 0;
+	if (dash_n[i] == '-')
+		i++;
+	else
+		return (0);
+	while (dash_n[i] == 'n')
+		i++;
+	if (dash_n[i] == '\0')
+		return (1);
+	return (0);
+}
 
 static void	print_args(int size, char **args, int out_fd)
 {
 	int	i;
 
 	i = 1;
-	if (ft_strcmp(args[1], "-n") == 0)
+	if (valid_dash_n(args[1]))
 		i++;
 	while (i < size - 1)
 	{
@@ -44,9 +60,9 @@ int	ft_echo(t_command cmd)
 		ft_putstr_fd("\n", out);
 	else
 	{
-		comparison = ft_strcmp(cmd.args[1], "-n");
+		comparison = valid_dash_n(cmd.args[1]);
 		print_args(cmd.number_of_args, cmd.args, out);
-		if (comparison != 0)
+		if (comparison == 0)
 			ft_putstr_fd("\n", out);
 	}
 	if (g_minishell.on_fork)
